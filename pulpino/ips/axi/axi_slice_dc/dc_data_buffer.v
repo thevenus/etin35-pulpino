d1ac8620272a3a3daef3364d10ad8553f9479376
thevenus <fuad.mammadovf7@gmail.com>
2024-04-18 10:30:16 +0200

    Modified dc_data_buffer code to make it synthesizable
    Previously the function defined for log2 caused the Genus complain
    about passing the maximum number of loop unrolling limit. Even when we
    increased that limit the code didn't synthesize. So, log2 function is now
    removed and instead a macro is used. Haven't verified this in simulation yet.

--- pulpino/ips/axi/axi_slice_dc/dc_data_buffer.v
+++ pulpino/ips/axi/axi_slice_dc/dc_data_buffer.v
@@ -13,18 +13,7 @@ module dc_data_buffer(clk, rstn, write_pointer, write_data, read_pointer, read_d
     parameter DATA_WIDTH   = 32;
     parameter BUFFER_DEPTH = 8;
 
-    `ifndef PULP_FPGA_EMUL
-        function integer log2(input integer value);
-        begin
-            value = value - 1;
-            for (log2 = 0; value > 0; log2 = log2 + 1)
-                value = value >> 1;
-        end
-        endfunction
-        `define log2(N) log2(N)
-    `else
-        `define log2(N) ((N)<=(1) ? 0 : (N)<=(2) ? 1 : (N)<=(4) ? 2 : (N)<=(8) ? 3 : (N)<=(16) ? 4 : (N)<=(32) ? 5 : (N)<=(64) ? 6 : (N)<=(128) ? 7 : (N)<=(256) ? 8 : (N)<=(512) ? 9 : (N)<=(1024) ? 10 : -1)
-    `endif
+    `define log2(N) ((N)<=(1) ? 0 : (N)<=(2) ? 1 : (N)<=(4) ? 2 : (N)<=(8) ? 3 : (N)<=(16) ? 4 : (N)<=(32) ? 5 : (N)<=(64) ? 6 : (N)<=(128) ? 7 : (N)<=(256) ? 8 : (N)<=(512) ? 9 : (N)<=(1024) ? 10 : -1)
 
     input                          clk;
     input                          rstn;
