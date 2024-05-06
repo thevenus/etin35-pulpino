5a675cd27a103fcc40e2fdc97e78e5cfb14347ab
ViktorStromberg <vi0873st-s@student.lu.se>
2024-05-06 09:58:54 +0200

    Adding 3 seperate clock domains for top, SPI, JTAG

--- synthesis/scripts/create_clock.tcl
+++ synthesis/scripts/create_clock.tcl
@@ -26,3 +26,41 @@ set_attribute slew_fall $ClkFall_uncertainty $ClkName
  
 external_delay -input $InputDelay  -clock [find / -clock $ClkName] -name in_con  [find /des* -port ports_in/*]
 external_delay -output $OutputDelay -clock [find / -clock $ClkName] -name out_con [find /des* -port ports_out/*]
+
+# SPI Slave clock
+set PERIOD_SPI 100000
+set ClkName_SPI clk_spi
+
+define_clock -name $ClkName_SPI -period $PERIOD_SPI -design $ClkTop -domain $ClkDomain [find / -port spi_clk_i*]
+
+set_attribute clock_network_late_latency $ClkLatency $ClkName_SPI
+set_attribute clock_source_late_latency  $ClkLatency $ClkName_SPI 
+
+set_attribute clock_setup_uncertainty $ClkLatency $ClkName_SPI
+set_attribute clock_hold_uncertainty $ClkLatency $ClkName_SPI 
+
+set_attribute slew_rise $ClkRise_uncertainty $ClkName_SPI 
+set_attribute slew_fall $ClkFall_uncertainty $ClkName_SPI
+ 
+external_delay -input $InputDelay  -clock [find / -clock $ClkName_SPI] -name in_con  [find /des* -port ports_in/*]
+external_delay -output $OutputDelay -clock [find / -clock $ClkName_SPI] -name out_con [find /des* -port ports_out/*]
+
+# JTAG clock
+set PERIOD_JTAG 100000
+set ClkName_JTAG clk_jtag
+
+define_clock -name $ClkName_JTAG -period $PERIOD_JTAG -design $ClkTop -domain $ClkDomain [find / -port tck_i*]
+
+set_attribute clock_network_late_latency $ClkLatency $ClkName_JTAG
+set_attribute clock_source_late_latency  $ClkLatency $ClkName_JTAG 
+
+set_attribute clock_setup_uncertainty $ClkLatency $ClkName_JTAG
+set_attribute clock_hold_uncertainty $ClkLatency $ClkName_JTAG
+
+set_attribute slew_rise $ClkRise_uncertainty $ClkName_JTAG 
+set_attribute slew_fall $ClkFall_uncertainty $ClkName_JTAG
+ 
+external_delay -input $InputDelay  -clock [find / -clock $ClkName_JTAG] -name in_con  [find /des* -port ports_in/*]
+external_delay -output $OutputDelay -clock [find / -clock $ClkName_JTAG] -name out_con [find /des* -port ports_out/*]
+
+set_clock_groups -asynchronous -group $ClkName -group $ClkName_SPI -group $ClkName_JTAG
