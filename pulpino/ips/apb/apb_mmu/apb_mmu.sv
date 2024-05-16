600acbf4b563fd94416691fca3014227323e3a55
thevenus <fuad.mammadovf7@gmail.com>
2024-05-16 20:16:44 +0200

    Initial implementation of APB MMU wrapper done
    APB MMU FSM implementation is complete. TB has also
    been implemented and the module has been checked
    by loading 5 matrices for calculation. All results
    have been successful.

--- pulpino/ips/apb/apb_mmu/apb_mmu.sv
+++ pulpino/ips/apb/apb_mmu/apb_mmu.sv
@@ -1,4 +1,10 @@
 // APB Wrapper for our Jollof MMU module
+`define REG_MMU_CTRL            10'b0000000000 //size of data ram in multiples of 8 kBye
+`define REG_MMU_STATUS          10'b0000000001 //size of data ram in multiples of 8 kBye
+`define REG_MMU_INDATA_START    10'b0001000000 //size of data ram in multiples of 8 kBye
+`define REG_MMU_INDATA_STOP     10'b0001111111 //size of data ram in multiples of 8 kBye
+`define REG_MMU_OUTDATA_START   10'b0010000000 //size of data ram in multiples of 8 kBye
+`define REG_MMU_OUTDATA_STOP    10'b0100011111 //size of data ram in multiples of 8 kBye
 
 module apb_mmu 
 (
@@ -22,9 +28,13 @@ module apb_mmu
                       
                         
     logic [31:0]    indata_reg [64], indata_next [64];
-    logic [31:0]    outdata_reg [160], outdata_next [160];
+    logic [17:0]    outdata_reg [160], outdata_next [160];
     logic [3:0]     ctrl_reg, ctrl_next;
-    logic           status_reg, status_next;
+    logic [1:0]     status_reg, status_next;
+    logic           clear_start_bit;
+    logic [9:0]     apb_addr_trimmed;
+
+    assign apb_addr_trimmed = PADDR[11:2];
 
     // sequential part
     always_ff @(posedge HCLK, negedge HRESETn) 
@@ -39,7 +49,7 @@ module apb_mmu
             end
 
             ctrl_reg <= 4'b0;
-            status_reg <= 1'b0;
+            status_reg <= 2'b00;
         end else begin
             for (int i = 0; i<64; i++) begin
                 indata_reg[i] <= indata_next[i];
@@ -48,10 +58,10 @@ module apb_mmu
             for (int i = 0; i<160; i++) begin
                 outdata_reg[i] <= outdata_next[i];
             end
-
+            
             ctrl_reg <= ctrl_next;
             status_reg <= status_next;
-        end    
+        end
     end
  
     // write register logic
@@ -61,12 +71,16 @@ module apb_mmu
         ctrl_next = ctrl_reg;
 
         if (PSEL && PENABLE && PWRITE) begin
-            if (PADDR >= 32'h1A10_3100 && PADDR <= 32'h1A10_31FF) begin
-                indata_next[PADDR[7:2]] = PWDATA;
-            end else if (PADDR == 32'h1A10_3000) begin
-                ctrl_next = PWDATA;
+            if (apb_addr_trimmed >= `REG_MMU_INDATA_START && apb_addr_trimmed <= `REG_MMU_INDATA_STOP) begin
+                indata_next[apb_addr_trimmed[5:0]] = PWDATA;
+            end else if (apb_addr_trimmed == `REG_MMU_CTRL) begin
+                ctrl_next = PWDATA[3:0];
             end
         end
+
+        // clearing the start bit
+        if (clear_start_bit)
+            ctrl_next = {ctrl_reg[3:1], 1'b0};
     end
 
     // read register logic
@@ -75,22 +89,16 @@ module apb_mmu
         PRDATA = 32'b0;
 
         if (PSEL && PENABLE && !PWRITE) begin
-            if (PADDR >= 32'h1A10_3200 && PADDR <= 32'h1A10_3480) begin
-                PRDATA = outdata_reg[PADDR[7:2]];
-            end else if (PADDR >= 32'h1A10_3100 && PADDR <= 32'h1A10_31FF) begin
-                PRDATA = indata_reg[PADDR[7:2]];
-            end else if (PADDR == 32'h1A10_3000) begin 
-                PRDATA = ctrl_reg;
-            end else if (PADDR == 32'h1A10_3480) begin
-                PRDATA = status_reg;
+            if (apb_addr_trimmed >= `REG_MMU_OUTDATA_START && apb_addr_trimmed <= `REG_MMU_OUTDATA_STOP) begin
+                PRDATA = {14'b0, outdata_reg[apb_addr_trimmed[5:0]]};
+            end else if (apb_addr_trimmed >= `REG_MMU_INDATA_START && apb_addr_trimmed <= `REG_MMU_INDATA_STOP) begin
+                PRDATA = indata_reg[apb_addr_trimmed[5:0]];
+            end else if (apb_addr_trimmed == `REG_MMU_CTRL) begin 
+                PRDATA = {28'b0, ctrl_reg};
+            end else if (apb_addr_trimmed == `REG_MMU_STATUS) begin
+                PRDATA = {30'b0, status_reg};
             end
         end
-    end
-
-    always_comb
-    begin
-        status_next = status_reg;
-        outdata_next = outdata_reg;    
     end
 
     assign PREADY = 1'b1;
@@ -101,7 +109,7 @@ module apb_mmu
     // ████  ████ ████  ████ ██    ██ 
     // ██ ████ ██ ██ ████ ██ ██    ██ 
     // ██  ██  ██ ██  ██  ██ ██    ██ 
-    // ██      ██ ██      ██  ██████                        
+    // ██      ██ ██      ██  ██████   
 
     logic       [7:0]   mmu_input_data_i;
     logic       [17:0]  mmu_read_data_out_o;
@@ -121,11 +129,99 @@ module apb_mmu
         .finish             (mmu_finish_o)
     );
 
-    always_comb 
-    begin
-        if (ctrl_reg[0])
-        
-    end
+    // Wrapper FSM
+    parameter s_IDLE        = 3'b000,
+              s_GIVE_INPUT  = 3'b001,
+              s_WAIT_CALC   = 3'b010,
+              s_START_READ  = 3'b011,
+              s_READ_RAM    = 3'b100;
+    
+    logic   [2:0]   state_reg, state_next;
+    logic   [7:0]   cnt_reg, cnt_next;
+    logic   [2:0]   num_mat_reg, num_mat_next;
 
     
+    always_ff @( posedge HCLK, negedge HRESETn ) 
+    begin
+        if (!HRESETn) begin
+            state_reg <= s_IDLE;
+            cnt_reg <= 8'b0;
+            num_mat_reg <= 3'b0;
+        end else begin
+            state_reg <= state_next;
+            cnt_reg <= cnt_next;
+            num_mat_reg <= num_mat_next;
+        end
+    end
+
+    always_comb
+    begin
+        state_next = state_reg;
+        mmu_valid_input_i = 1'b0;
+        mmu_read_ram_i = 1'b0;
+        mmu_input_data_i = 8'b0;
+        cnt_next = cnt_reg;
+        num_mat_next = num_mat_reg;
+        status_next = status_reg;
+        outdata_next = outdata_reg;
+        clear_start_bit = 1'b0;
+
+        case (state_reg)
+            s_IDLE: begin
+                cnt_next = 8'b0;
+                num_mat_next = ctrl_reg[3:1];
+
+                // if the start bit is set and number of matrices to be multiplied is bigger than 0
+                if (ctrl_reg[0] == 1'b1 && num_mat_reg != 3'b000) begin
+                    state_next = s_GIVE_INPUT;
+                end
+            end
+
+            s_GIVE_INPUT: begin
+                mmu_valid_input_i = 1'b1;
+                mmu_input_data_i = indata_reg[cnt_reg[7:2]][(24-(cnt_reg[1:0]<<3)) +: 8];
+                clear_start_bit = 1'b1;
+                cnt_next = cnt_reg + 1;
+
+                if (cnt_reg[4:0] == 8'd31) begin
+                    state_next = s_WAIT_CALC;                   
+                end
+            end
+
+            s_WAIT_CALC: begin
+                mmu_valid_input_i = 1'b0;
+
+                if (mmu_finish_o) begin
+                    if (num_mat_reg > 3'd1) begin
+                        num_mat_next = num_mat_reg - 1;
+                        state_next = s_GIVE_INPUT;
+                    end else begin
+                        state_next = s_START_READ;
+                        cnt_next = 8'b0;
+                    end
+                end
+            end
+
+            s_START_READ: begin
+                mmu_read_ram_i = 1'b1;
+
+                if (mmu_read_data_out_o != 18'b0) begin
+                    outdata_next[cnt_reg] = mmu_read_data_out_o;
+                    cnt_next = cnt_reg + 1;
+                    state_next = s_READ_RAM;
+                end
+            end
+
+            s_READ_RAM: begin
+                outdata_next[cnt_reg] = mmu_read_data_out_o;
+
+                if (cnt_reg == 8'd159) begin
+                    status_next = 2'b01;
+                    state_next = s_IDLE;
+                end else begin
+                    cnt_next = cnt_reg + 1;
+                end
+            end
+        endcase
+    end
 endmodule
--- pulpino/tb/tb_apb.sv
+++ pulpino/tb/tb_apb.sv
@@ -1,3 +1,4 @@
+`timescale 1ns/1ps
 module tb_apb;
     parameter s_IDLE = 2'b00, s_SETUP = 2'b01, s_ACCESS = 2'b10;
 
@@ -6,7 +7,7 @@ module tb_apb;
 
     logic   [31:0]  paddr = 0;
     logic   [31:0]  pwdata = 0;
-    logic           pwrite = 0;
+    logic           pwrite;
     logic           psel = 0;
     logic           pena = 0;
     logic   [31:0]  prdata;
@@ -33,28 +34,58 @@ module tb_apb;
 
     always #50ns clk = ~clk;
 
+    logic   [7:0] input_stimuli [256];
+    integer fd, i;
     initial begin 
         rst_n = 1'b0; start_transfer = 1'b0;
         paddr = 0;
-        pwdata = 0;
-        pwrite = 1'b0;
-
-        #200ns;
-        rst_n = 1'b1; start_transfer = 1'b1;
-        paddr = 32'h1A10_3100; // boot address
-        pwdata = 32'hFACE_DEAD; // random data
         pwrite = 1'b1;
 
         #200ns;
-        start_transfer = 1'b0;
+        rst_n = 1'b1;
+        
+        #20000ns;
+        // read input stimuli txt into the array
+        fd = $fopen("/h/d9/n/fu6315ma-s/Documents/jollof_mmu/functional_model_stimuli/input_stimuli_wo_0.txt", "r");
+        i = 0;
+        while (!$feof(fd)) begin
+            $fscanf(fd, "%b", input_stimuli[i]);
+            i++;
+        end
 
         #1000ns;
-        start_transfer = 1'b1;
-        paddr = 32'h1A10_3100; // boot address
-        pwrite = 1'b0;
+        $fclose(fd);
 
-        #200ns;
+        // give the input to the apb_mmu
+        start_transfer = 1'b1;
+        for (i = 0; i<160; i=i+4) begin
+            wait(state_reg == s_SETUP);
+            pwdata  = {input_stimuli[i], input_stimuli[i+1], input_stimuli[i+2], input_stimuli[i+3]};
+            paddr   = 'h1A10_3100 + i;
+            wait (state_reg == s_ACCESS);
+        end
         start_transfer = 1'b0;
+
+        #1000ns
+
+        // set start bit and number of matrices to be multiplied
+        start_transfer = 1'b1;
+        paddr = 'h1A10_3000;
+        pwdata = {28'b0, 3'b101, 1'b1};
+        wait(state_reg == s_SETUP);
+        start_transfer = 1'b0;
+        wait(state_reg == s_IDLE);
+
+        // wait for the done bit
+        start_transfer = 1'b1;
+        pwrite = 1'b0;
+        paddr = 'h1A10_3004;
+        wait(prdata[0] == 1'b1);
+        start_transfer = 1'b0;
+        
+        // calculation is done! check the answer!
+
+        $stop();
     end
 
     always @(posedge clk) begin
@@ -81,6 +112,7 @@ module tb_apb;
                 if (start_transfer)
                     state_next = s_SETUP;
             end
+            
             s_SETUP : begin
                 // outputs
                 psel = 1'b1;
@@ -89,14 +121,19 @@ module tb_apb;
                 // state transition
                 state_next = s_ACCESS;
             end
+            
             s_ACCESS : begin
                 // outputs
                 psel = 1'b1;
                 pena = 1'b1;
 
                 // state transition
-                if (pready)
-                    state_next = s_IDLE;
+                if (pready) begin
+                    if (start_transfer)
+                        state_next = s_SETUP;
+                    else
+                        state_next = s_IDLE;
+                end
             end
         endcase
     end
