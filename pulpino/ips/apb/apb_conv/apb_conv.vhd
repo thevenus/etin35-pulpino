d7808e2d03bd9355cf30029cf59bcf85b085b9d0
ViktorStromberg <vi0873st-s@student.lu.se>
2024-06-02 17:28:49 +0200

    Updated apb_conv_ctrl and created apb_conv which is top file.

--- pulpino/ips/apb/apb_conv/apb_conv.vhd
+++ pulpino/ips/apb/apb_conv/apb_conv.vhd
@@ -1,1 +1,109 @@
 -- Top module for apb_conv connecting controller and convolve unit 
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all;
+USE IEEE.numeric_std.all;
+
+entity apb_conv is
+    port(
+        -- APB BUS
+        HCLK        : in std_logic;
+        HRESETn     : IN STD_LOGIC;
+        PADDR       : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+        PWDATA      : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+        PWRITE      : IN STD_LOGIC;
+        PSEL        : IN STD_LOGIC;
+        PENABLE     : IN STD_LOGIC;
+        PRDATA      : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        PREADY      : OUT STD_LOGIC;
+        PSLVERR     : OUT STD_LOGIC
+        );
+end apb_conv;
+
+architecture structural of apb_conv is
+
+component conv_ctrl is
+    port(
+        HCLK        : in std_logic;
+        HRESETn     : IN STD_LOGIC;
+        PADDR       : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+        PWDATA      : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+        PWRITE      : IN STD_LOGIC;
+        PSEL        : IN STD_LOGIC;
+        PENABLE     : IN STD_LOGIC;
+        PRDATA      : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        PREADY      : OUT STD_LOGIC;
+        PSLVERR     : OUT STD_LOGIC;
+        conv_res_i      : in std_logic_vector(7 downto 0);
+        conv_res_rdy_i  : in std_logic;
+        conv_nrow_i     : in std_logic_vector(4 downto 0);
+        conv_ncol_i     : in std_logic_vector(4 downto 0);
+        conv_nchn_i     : in std_logic_vector(1 downto 0);
+        input_window_o  : out std_logic_vector(74 downto 0);
+        filter_o        : out std_logic_vector(74 downto 0);
+        valid_data_o    : out std_logic
+        );
+end component;
+
+component convolve is
+    port(
+        clk : in std_logic;
+        rst_n : in std_logic;
+        valid_data: in std_logic;
+        input_data : in std_logic_vector(74 downto 0);
+        filter_data : in std_logic_vector(74 downto 0);
+        result : out std_logic_vector(7 downto 0);
+        ready : out std_logic;
+        n_row : out std_logic_vector(4 downto 0);
+        n_col : out std_logic_vector(4 downto 0);
+        n_chn : out std_logic_vector(1 downto 0)
+        );
+end component;
+
+signal Conv_res : std_logic_vector(7 downto 0);
+signal Conv_res_rdy : std_logic; 
+signal Conv_nrow : std_logic_vector(4 downto 0);
+signal Conv_ncol : std_logic_vector(4 downto 0);
+signal Conv_nchn : std_logic_vector(1 downto 0);
+signal Input_window : std_logic_vector(74 downto 0);
+signal Filter : std_logic_vector(74 downto 0);
+signal Valid_data : std_logic;
+
+begin
+
+conv_ctrl_inst : conv_ctrl
+    port map(
+            HCLK => HCLK,
+            HRESETn => HRESETn,
+            PADDR => PADDR,
+            PWDATA => PWDATA,
+            PWRITE => PWRITE,
+            PSEL => PSEL,
+            PENABLE => PENABLE,
+            PRDATA => PRDATA,
+            PREADY => PREADY,
+            PSLVERR => PSLVERR,
+            conv_res_i => Conv_res,
+            conv_res_rdy_i => Conv_res_rdy,
+            conv_nrow_i => Conv_nrow,
+            conv_ncol_i => Conv_ncol,
+            conv_nchn_i => Conv_nchn,
+            input_window_o => Input_window,
+            filter_o => Filter,
+            valid_data_o => Valid_data
+            );
+
+convolve_inst : convolve
+    port map(
+            clk => HCLK,
+            rst_n => HRESETn,
+            valid_data => Valid_data,
+            input_data => Input_window,
+            filter_data => Filter,
+            result => Conv_res,
+            ready => Conv_res_rdy,
+            n_row => Conv_nrow,
+            n_col => Conv_ncol,
+            n_chn => Conv_nchn
+            );
+
+end structural; 
--- pulpino/ips/apb/apb_conv/conv_ctrl.vhd
+++ pulpino/ips/apb/apb_conv/conv_ctrl.vhd
@@ -2,6 +2,7 @@
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.all;
 USE IEEE.numeric_std.all;
+USE IEEE.std_logic_unsigned.all;
 
 entity conv_ctrl is 
     port (
@@ -25,7 +26,7 @@ entity conv_ctrl is
         conv_nchn_i     : in std_logic_vector(1 downto 0);
         input_window_o  : out std_logic_vector(74 downto 0);
         filter_o        : out std_logic_vector(74 downto 0);
-        valid_data_o    : out std_logic;
+        valid_data_o    : out std_logic
         );
 end conv_ctrl;
 
@@ -40,16 +41,15 @@ architecture Behavioural of conv_ctrl is
     signal clear_start_bit : std_logic;
     signal indata_row_cnt, indata_row_cnt_next : unsigned(2 downto 0);
     signal indata_rx_cnt, indata_rx_cnt_next : unsigned(1 downto 0);
-    signal apb_addr_trimmed : std_logic(9 downto 0);
+    signal apb_addr_trimmed : std_logic_vector(9 downto 0);
     signal filter_reg, filter_reg_next: std_logic_vector(74 downto 0);
-    
-    apb_addr_trimmed <= PADDR(11 downto 2);
 
     -- conv controller signals
     type state_type is (s_WAIT_DATA, s_WAIT_CALC, s_STORE_RESULT);
     signal state_reg, state_next : state_type;
     signal outdata_cnt_reg, outdata_cnt_next : unsigned(9 downto 0);
-    signal temp_outdata_reg, temp_outdata_next : unsigned(7 downto 0);
+    signal temp_outdata_reg, temp_outdata_next : unsigned(8 downto 0);
+
 begin
 
     --  █████  ██████  ██████  
@@ -57,7 +57,8 @@ begin
     -- ███████ ██████  ██████  
     -- ██   ██ ██      ██   ██ 
     -- ██   ██ ██      ██████  
-
+    
+    apb_addr_trimmed <= PADDR(11 downto 2);
     PREADY <= '1';
     PSLVERR <= '0';
 
@@ -69,14 +70,14 @@ begin
                 indata_reg <= (others => (others => '0'));
                 indata_row_cnt <= "010";
                 indata_rx_cnt <= (others => '0');
-                ctrl_reg <= (others => '0');
+                ctrl_reg <= '0';
                 status_reg <= (others => '0');
                 outdata_reg <= (others => (others => '0'));
                 filter_reg <= (others => '0');
             else 
                 indata_reg <= indata_next;
                 indata_row_cnt <= indata_row_cnt_next;
-                indata_rx_cnt <= indata_rx_count_next;
+                indata_rx_cnt <= indata_rx_cnt_next;
                 ctrl_reg <= ctrl_next;
                 status_reg <= status_next;
                 outdata_reg <= outdata_next;
@@ -94,43 +95,60 @@ begin
         filter_reg_next <= filter_reg;
         ctrl_next <= ctrl_reg;
 
-        if (PSEL = '1' and PENABLE = '1' and PWRITE = '1' and indata_row_cnt < "011100") then -- indata_row_cnt = 28
-            if (PADDR = x"1A103500") then
+        if (PSEL = '1' and PENABLE = '1' and PWRITE = '1' and indata_row_cnt < "011100") then -- indata_row_cnt < 28
+            if (PADDR = x"1A103500") then -- ctrl register address
                 ctrl_next <= PWDATA(0);
-            elsif (PADDR = x"1A103508") then
-                if (indata_row_cnt > "100") then
+            elsif (PADDR = x"1A103508") then -- indata address
+                if (indata_row_cnt > "100" and conv_nrow_i < "11010") then -- conv_nrow <26
                     indata_next(0) <= indata_reg(1);
                     indata_next(1) <= indata_reg(2);
                     indata_next(2) <= indata_reg(3);
-                    indata_next(4) <= indata_reg(5);
-                    indata_next(5)(89 downto 62) <= PWDATA(27 downto 0);
+                    indata_next(3) <= indata_reg(4);
+                    indata_next(4)(89 downto 62) <= PWDATA(27 downto 0);
                     indata_row_cnt <= "100";
                     indata_rx_cnt_next <= "01";
+                elsif (indata_row_cnt > "100" and conv_nrow_i = "11010") then -- conv_nrow = 26
+                    indata_next(0) <= indata_reg(1);
+                    indata_next(1) <= indata_reg(2);
+                    indata_next(2) <= indata_reg(3);
+                    indata_next(3) <= indata_reg(4);
+                    indata_next(4) <= (others => '0');
+                    indata_row_cnt <= "101";
+                    indata_rx_cnt_next <= (others => '0');
+                elsif (indata_row_cnt > "100" and conv_nrow_i = "11011") then -- conv_nrow = 27
+                    indata_next(0) <= indata_reg(1);
+                    indata_next(1) <= indata_reg(2);
+                    indata_next(2) <= indata_reg(3);
+                    indata_next(3) <= (others => '0');
+                    indata_next(4) <= (others => '0');
+                    indata_row_cnt <= "010";
+                    indata_rx_cnt_next <= (others => '0');                
                 else
                     if (indata_rx_cnt = "00") then
-                        indata_next(indata_row_cnt)(89 downto 62) <= PWDATA(27 downto 0);
+                        indata_next(to_integer(indata_row_cnt))(89 downto 62) <= PWDATA(27 downto 0);
                         indata_rx_cnt_next <= indata_rx_cnt + 1;
                     elsif (indata_rx_cnt = "01") then
-                        indata_next(indata_row_cnt)(61 downto 34) <= PWDATA(27 downto 0);
+                        indata_next(to_integer(indata_row_cnt))(61 downto 34) <= PWDATA(27 downto 0);
                         indata_rx_cnt_next <= indata_rx_cnt + 1;
                     elsif (indata_rx_cnt = "10") then
-                        indata_next(indata_row_cnt)(33 downto 6) <= PWDATA(27 downto 0);
+                        indata_next(to_integer(indata_row_cnt))(33 downto 6) <= PWDATA(27 downto 0);
                         indata_rx_cnt_next <= (others => '0');
                         indata_row_cnt_next <= indata_row_cnt + 1;
                     end if;
                 end if;
-            elsif (PADDR = x"1A103900") then
+            elsif (PADDR = x"1A103900") then -- filter reg 1 address
                 filter_reg_next(74 downto 45) <= PWDATA(29 downto 0);
-            elsif (PADDR = x"1A103904") then
+            elsif (PADDR = x"1A103904") then -- filter reg 2 address
                 filter_reg_next(44 downto 15) <= PWDATA(29 downto 0);
-            elsif (PADDR = x"1A103908") then
+            elsif (PADDR = x"1A103908") then -- filter reg 3 address
                 filter_reg_next(14 downto 0) <= PWDATA(14 downto 0);
             end if;
         end if;
         
         if (clear_start_bit = '1') then
-            ctrl_next(0) <= '1';
+            ctrl_next <= '1';
         end if;
+
     end process apb_write_logic;
 
     -- read register logic
@@ -138,18 +156,18 @@ begin
     begin
         PRDATA <= (others => '0');
         if (PSEL = '1' and PENABLE = '1' and PWRITE = '0') then
-            if (PADDR = x"1A103500") then
+            if (PADDR = x"1A103500") then -- ctrl reg address
                 PRDATA <= "0000000000000000000000000000000" & ctrl_reg;
-            elsif (PADDR = x"1A103504") then
+            elsif (PADDR = x"1A103504") then -- status reg address
                 PRDATA <= "00000000000000000000000000000" & status_reg;
-            elsif (PADDR  >= x"1A10350C" and PADDR <= x"1A103818") then
-                PRDATA <= outdata_reg(PADDR(11 downto 2) - 323);
-            elsif (PADDR = x"1A103900") then
-                PRDATA <= filter_reg(74 downto 45);
-            elsif (PADDR = x"1A103904") then
-                PRDATA <= filter_reg(44 downto 15);
-            elsif (PADDR = x"1A103908") then
-                PRDATA <= filter_reg(14 downto 0);
+            elsif (PADDR  >= x"1A10350C" and PADDR <= x"1A103818") then -- outdata reg addresses
+                PRDATA <= outdata_reg(to_integer(unsigned(PADDR(11 downto 2)) - "101000011"));--323);
+--            elsif (PADDR = x"1A103900") then -- filter reg 1 address
+--                PRDATA <= filter_reg(74 downto 45);
+--            elsif (PADDR = x"1A103904") then -- filter reg 2 address
+--                PRDATA <= filter_reg(44 downto 15);
+--            elsif (PADDR = x"1A103908") then -- filter reg 3 address
+--                PRDATA <= filter_reg(14 downto 0);
             end if;
         end if;
     end process;
@@ -159,15 +177,17 @@ begin
     -- ██      ██    ██ ██ ██  ██ ██    ██     ██         ██    ██████  ██      
     -- ██      ██    ██ ██  ██ ██  ██  ██      ██         ██    ██   ██ ██      
     --  ██████  ██████  ██   ████   ████        ██████    ██    ██   ██ ███████ 
+
     filter_o <= filter_reg;
 
+    --update column data process
     process (conv_ncol_i, indata_reg)
         variable msb_pos: integer;
         variable lsb_pos: integer; 
     begin
-        msb_pos := 95 - conv_ncol_i * 3;
-        lsb_pos := 81 - conv_ncol_i * 3;
-        input_window_o <= indata_reg(0)(msb_pos downto lsb_pos);
+        msb_pos := 95 - (unsigned(conv_ncol_i) * 3);
+        lsb_pos := 81 - (unsigned(conv_ncol_i) * 3);
+        input_window_o <= indata_reg(0)(msb_pos downto lsb_pos) & indata_reg(1)(msb_pos downto lsb_pos) & indata_reg(2)(msb_pos downto lsb_pos) & indata_reg(3)(msb_pos downto lsb_pos) & indata_reg(4)(msb_pos downto lsb_pos);
     end process;
 
     process (HCLK, HRESETn)
@@ -195,7 +215,7 @@ begin
 
         case state_reg is 
             when s_WAIT_DATA =>
-                if (ctrl_reg = '1') then
+                if (ctrl_reg = '1') then -- start = 1
                     clear_start_bit <= '1';
                     status_next <= "000";
                     state_next <= s_WAIT_CALC;
@@ -207,14 +227,34 @@ begin
                 valid_data_o <= '1';
                 if (conv_res_rdy_i = '1') then
                     valid_data_o <= '0';
-                    temp_outdata_next <= outdata_reg(outdata_cnt_reg(9 downto 2))(outdata_cnt_reg())
+                    temp_outdata_next <= outdata_reg(outdata_cnt_reg(9 downto 2))(outdata_cnt_reg(1 downto 0)) + conv_res_i;
+                    state_next <= s_STORE_RESULT;
                 else
                     state_next <= s_WAIT_CALC;
                 end if;
             
             when s_STORE_RESULT =>
-                
-                
+                outdata_cnt_next <= outdata_cnt_reg + 1;
+                if (temp_outdata_reg > "011111111") then -- 255
+                    outdata_next(to_integer(outdata_cnt_reg(9 downto 2)))(to_integer(outdata_cnt_reg(1 downto 0))) <= "11111111";
+                else
+                    outdata_next(to_integer(outdata_cnt_reg(9 downto 2)))(to_integer(outdata_cnt_reg(1 downto 0))) <= temp_outdata_reg(7 downto 0);
+                end if;
+                if (conv_ncol_i = "11011") then --27
+                    state_next <= s_WAIT_CALC;
+                else
+                    status_next(1) <= '0'; -- row_done_bit <= '1'
+                    if (conv_nrow_i = "11011") then -- 27
+                        status_next(2) <= '1'; -- channel_done_bit <= '1'
+                        outdata_cnt_next <= (others => '0');
+                        state_next <= s_WAIT_DATA;
+                        if (conv_nchn_i = "10") then
+                            status_next(0) <= '1'; -- done_bit <= '1'
+                        end if;
+                    else
+                        state_next <= s_WAIT_DATA;
+                    end if;
+                end if;
         end case;
 
     end process;
